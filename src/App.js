e417a3318c3af82a6b0952eab98749123f2f9df2
Gugu Mabuza <mabuzagu@gmail.com>
2020-11-30 22:18:24 -0500

    Finished basic shop page

--- src/App.js
+++ src/App.js
@@ -4,17 +4,14 @@ import './App.css';
 
 import HomePage from './pages/homepage/homepage-component'
 
-
-const HatsPage = () => (
-  <h1>Hat Page</h1>
-);
+import ShopPage from './pages/shop/shop.component'
 
 function App() {
   return (
     <div>
       <Switch>
         <Route exact path='/' component={HomePage} />
-        <Route path='/hats' component={HatsPage} />
+        <Route path='/shop' component={ShopPage} />
       </Switch>
     </div>
   );
